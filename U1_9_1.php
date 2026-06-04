32daa2ebf5cbf34d6668d406e381e5919c1762ad
MEXTTT18 <meetrangani35@gmail.com>
2026-06-04 11:04:35 +0530

    Add files via upload

---
+++ U1_9_1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+    $arr = array('Ashit','rajubhai','cahman','jetho');
+
+    $reverse = array_reverse($arr);
+
+    foreach($reverse as $p){
+    echo $p."<br>";
+    }
+
+    $arr = array(
+        "name" => "Ashit",
+        "city" => "Rajkot",
+        "age" => 20
+    );
+
+    $reverse = array_reverse($arr);
+
+    foreach($reverse as $key => $value){
+        echo $key . " : " . $value . "<br>";
+    }
+
+    $arr = array(
+        array("Ashit", 20, "Rajkot"),
+        array("Rahul", 22, "Ahmedabad"),
+        array("Amit", 21, "Surat")
+    );
+
+    $reverse = array_reverse($arr);
+
+    foreach($reverse as $row){
+        foreach($row as $value){
+            echo $value . " ";
+        }
+        echo "<br>";
+    }
+
+
+    ?>
+</body>
+</html>
