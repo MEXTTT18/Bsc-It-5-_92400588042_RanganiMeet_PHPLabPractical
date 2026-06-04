defd375bb650f88fbedf0b9014dbdf08647cb7f0
MEXTTT18 <meetrangani35@gmail.com>
2026-06-04 10:49:49 +0530

    Add files via upload

---
+++ U1_2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $mx = max(10,55,54);
+        echo"Max Is =".$mx."<br>";
+        $mn = min(10,55,54);
+        echo"Min Is =".$mn."<br>";
+    ?>
+</body>
+</html>
